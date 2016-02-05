3a3156878d66c8205bffd81648e7025dfaa99751
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-05 17:43:07 +0100

    Test user sign up

--- spec/features/view_spec.rb
+++ spec/features/view_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe "Views", type: :feature do
   describe "Visit home page", type: :feature do
     it "Visits home page" do
       visit root_path
-      expect(page).to have_content ("Search For Flights")
+      expect(page).to have_content "Search For Flights"
     end
   end
 
   describe "Visit sign in", type: :feature do
     it "Visits login page" do
-      visit  login_path
+      visit login_path
       expect(page).to have_content "Sign In"
     end
   end
 
   describe "Visit sign in", type: :feature do
     it "Visits sign up page" do
-      visit  new_user_path
+      visit new_user_path
       expect(page).to have_content "Sign Up"
     end
   end
