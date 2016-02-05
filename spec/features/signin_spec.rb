bd0e80457303ed7296873d6a9a291034b0b9710e
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-05 16:11:12 +0100

    Test user sign up

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,15 @@
+describe "the signin process", :type => :feature do
+  before :each do
+    @user = create(:user)
+  end
+
+  it "signs me in" do
+    visit login_path
+    within(".signup-container") do
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: @user.password
+    end
+    click_button "Sign In"
+    expect(page).to have_content "Welcome"
+  end
+end
--- spec/features/view_spec.rb
+++ spec/features/view_spec.rb
@@ -1,10 +1,24 @@
 require "rails_helper"
 
-RSpec.describe "Signup", type: :feature do
+RSpec.describe "Views", type: :feature do
   describe "Visit home page", type: :feature do
     it "Visits home page" do
       visit root_path
       expect(page).to have_content ("Search For Flights")
+    end
+  end
+
+  describe "Visit sign in", type: :feature do
+    it "Visits login page" do
+      visit  login_path
+      expect(page).to have_content "Sign In"
+    end
+  end
+
+  describe "Visit sign in", type: :feature do
+    it "Visits sign up page" do
+      visit  new_user_path
+      expect(page).to have_content "Sign Up"
     end
   end
 end
