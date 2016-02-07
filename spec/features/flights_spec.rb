bb90f2b10c08f27a0cd13dbe7053a1b8b140d3ff
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-07 21:23:09 +0100

    Test features

--- spec/features/flights_spec.rb
+++ spec/features/flights_spec.rb
@@ -9,24 +9,23 @@ describe "Flight", type: :feature do
     Seed.flights
   end
   context "Search Flight" do
-    it "Search for flight without date " do
+    it "Search for flight that doesnt exist " do
       visit "/"
       select "N'djili Airport Kinshasa, Congo", from: "departure_id"
-      select "Julius Nyerere International Airport Dar es Salam, Tanzania", from: "arrival_id"
-      fill_in('departure_date', with: "1990 - 11 - 1")
+      select Airport.last.name, from: "arrival_id"
+      fill_in("departure_date", with: "1990 - 11 - 1")
       select 1, from: "seats"
-      click_button 'Search'
+      click_button "Search"
       expect(page).to have_content "No Flights Available For That Search"
     end
 
     it "Search for flight without date " do
-      flight =
       visit "/"
-      select Airport.last.name , from: "departure_id"
+      select Airport.last.name, from: "departure_id"
       select "Julius Nyerere International Airport Dar es Salam, Tanzania", from: "arrival_id"
       select 1, from: "seats"
-      click_button 'Search'
-      expect(page).to have_content "No Flights Available For That Search"
+      click_button "Search"
+      expect(page).to have_content "Julius Nyerere International Airport Dar es Salam, Tanzania"
     end
   end
 
--- spec/support/seed.rb
+++ spec/support/seed.rb
@@ -2,15 +2,16 @@ class Seed
   def self.airports
     airports = [
       { name: "N'djili Airport", location: "Kinshasa, Congo" },
-      { name: "Leon M'ba International Airport", location: "Libraville, Gabon" },
+      { name: "Leon M'ba International Airport", location: "Libraville, Gabon"
+        },
       { name: "Kigali International Airport", location: "Kigali, Rwanda" },
       { name: "Julius Nyerere International Airport",
         location: "Dar es Salam, Tanzania" },
       { name: "Addis Ababa Bole International Airport",
         location: "Addis Ababa, Ethiopia" }
     ]
-    @airlines = ["Dana Air", "Sosoliso", "Bell View", "Kenya Airways", "Arik Air",
-                "Virgin Atlantic", "Sname Air"]
+    @airlines = ["Dana Air", "Sosoliso", "Bell View", "Kenya Airways",
+                 "Arik Air", "Virgin Atlantic", "Sname Air"]
     airports.each do |airport|
       Airport.create(airport)
     end
@@ -18,30 +19,19 @@ class Seed
 
   def self.flights
     10.times do
-      id = [1,2,3].sample
-      arr_id = [4,5].sample
+      airports = Airport.order("RANDOM()")
       flight = Flight.new
       flight.duration = 12
       flight.airline = @airlines.sample
-      flight.departure_id = id
-      flight.arrival_id  = arr_id
+      flight.departure_id = airports.first.id
+      flight.arrival_id = airports.last.id
       flight.departure_date = Time.now.advance(
-                            days: rand(0..20), hours: rand(1..9), minutes: rand(0..60))
+        days: rand(0..20), hours: rand(1..9), minutes: rand(0..60))
       flight.departure_time = flight.departure_date.strftime("%I:%M %p")
       flight.economy = rand(10..50) * 1000
       flight.business = rand(51..100) * 1000
       flight.first =  rand(101..200) * 1000
       flight.save!
-    end
-  end
-
-  def self.bookings
-    5.times do
-      booking = Booking.new
-      booking.flight_id = 1
-      booking.user_id = 1
-      booking.seats = 2
-      booking.save
     end
   end
 end
