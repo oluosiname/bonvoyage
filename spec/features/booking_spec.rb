12647db2c8cdbc6dfe37d25a05a27bbd410356f4
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-08 11:39:42 +0100

    Manage User Booking
    Why?
    GIVEN that I am a registered user
    And I sign in
    WHEN I click on manage bookings
    I should be able to edit my booking details for flights that have not departed
    
    How?
    Create action in booking controller to edit booking for flight that is still available
    
    https://www.pivotaltracker.com/story/show/112728597
    
    [finishes #112728597]

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -15,7 +15,33 @@ describe "Booking", type: :feature do
   end
 
   context "Book Flight" do
-    it "Books a flight " do
+    it "Books a flight and allows user to search for past booking" do
+      visit login_path
+      within(".signup-container") do
+        fill_in "Email", with: @user.email
+        fill_in "Password", with: @user.password
+      end
+      click_button "Sign In"
+      visit all_flights_path
+      find(:xpath, "/html/body/div[2]/div[2]/div[6]/a").click
+      expect(page).to have_content "Passenger Details"
+      fill_in "booking_passengers_attributes_0_name", with: "Kpeace"
+      fill_in "booking_passengers_attributes_0_phone", with: "08037118709"
+      click_button "Book"
+      expect(page).to have_current_path(confirm_bookings_path)
+      within(".dropdown-menu") do
+        click_link "Past Bookings"
+      end
+      find(:xpath, "/html/body/div[2]/div[2]/div[9]/a").click
+      fill_in "booking_passengers_attributes_0_name", with: "Sname"
+      fill_in "booking_passengers_attributes_0_phone", with: "08087621887"
+      click_button "Update"
+      expect(page).to have_content "Booking Updated Successfully"
+    end
+  end
+
+  context "Book Flight" do
+    it "Books a flight and allows user to view and delete past booking " do
       visit login_path
       within(".signup-container") do
         fill_in "Email", with: @user.email
@@ -37,3 +63,5 @@ describe "Booking", type: :feature do
     end
   end
 end
+
+
