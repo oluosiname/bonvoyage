57fd486e549efcc91f1ed1b699ca3ed0f32dbb02
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-02-09 15:46:21 +0100

    update read me

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -9,11 +9,21 @@ RSpec.describe BookingsController do
     Seed.flights
   end
   context "After successful payment" do
-    it "updates booking payment status to true" do
+    it "updates booking payment status to true for signed-in user" do
       user = create(:user)
       booking = create(:booking)
       user.email = "oluosiname@gmail.com"
       booking.user_id = user.id
+      booking.flight = Flight.last
+      booking.save!
+      get :success, id: booking.id
+      expect(booking.payment_status).to be true
+      booking.destroy
+    end
+    it "updates booking payment status to true for anonymous user" do
+      user = create(:user)
+      booking = create(:booking)
+      booking.anon_email = "oluosiname@gmail.com"
       booking.flight = Flight.last
       booking.save!
       get :success, id: booking.id
